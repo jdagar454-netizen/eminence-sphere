1eddac3e78ee4140aaa4c9c98f262ff7827ffff7
Eminence Sphere <info@eminencesphere.com>
2026-05-28 13:05:47 +0530

    Configure Firebase credentials

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -6,12 +6,13 @@
 // You can get this configuration in your Firebase Console:
 // Settings icon (gear) -> Project settings -> Web Apps section.
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBmyZ6JLVJj0EjgXFTGOSHK-d7uK3yo8ww",
+  authDomain: "team-eminence.firebaseapp.com",
+  projectId: "team-eminence",
+  storageBucket: "team-eminence.firebasestorage.app",
+  messagingSenderId: "1045226217031",
+  appId: "1:1045226217031:web:8ab538893f0b0b19c39b62",
+  measurementId: "G-SK2H0EB65Q"
 };
 
 // Initialize Firebase if the SDK has loaded
