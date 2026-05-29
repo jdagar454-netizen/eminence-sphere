7eba94171514bbd4d28b97e56e676996dfcfdd6d
Eminence Sphere <info@eminencesphere.com>
2026-05-30 00:20:03 +0530

    Fix administration pipeline console script dependency error

---
+++ public/js/firebase-config.js
@@ -0,0 +1,18 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBmyZ6JLVJj0EjgXFTGOShK-d7uK3yo8ww",
+  authDomain: "team-eminence.firebaseapp.com",
+  projectId: "team-eminence",
+  storageBucket: "team-eminence.firebasestorage.app",
+  messagingSenderId: "1045226217031",
+  appId: "1:1045226217031:web:8ab538893f0b0b19c39b62",
+  measurementId: "G-SK2H0EB65Q"
+};
+
+// Initialize Firebase App
+if (typeof firebase !== 'undefined') {
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
+} else {
+  console.error("Firebase SDK not loaded prior to config script.");
+}
